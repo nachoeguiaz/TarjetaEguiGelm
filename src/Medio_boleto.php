e7cedf3486ee4d03cb3bd9c685d607bb50bb2f0b
nachoeguiaz <nachoeguiazu@gmail.com>
2016-11-03 16:09:06 -0300

    Update Medio_boleto.php

--- src/Medio_boleto.php
+++ src/Medio_boleto.php
@@ -1,15 +1,13 @@
 <?php
-
 namespace Tarjeta;
-
 class Medio_boleto extends Tarjetas_comun{	
-	public function __construct(){
+	public function __construct($id){
+		$this->id = $id;
+		$this->plus = 0;
 		$this->plata = 0;
 		$this->boleto_cole = 4;
 		$this->boleto_bici = 6;
 		$this->transbordo = (float)((int)($this->boleto_cole/3*100)/100);
 	}
 }
-
-
 ?>
